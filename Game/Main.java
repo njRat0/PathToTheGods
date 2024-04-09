73ce9721e956d5c08fb523ecfa76aada92cbb233
njRat0 <oleksandr10001@gmail.com>
2024-04-09 11:40:08 -0400

    basic window

--- Game/Main.java
+++ Game/Main.java
@@ -4,5 +4,6 @@ public class Main {
     static public void main(String[] arg){
         Window window = new Window();
         window.CreateWindow();
+        System.out.println(window.size());
     }
 }
--- Game/Window.java
+++ Game/Window.java
@@ -2,19 +2,16 @@ package PathToTheGods.Game;
 
 import javax.swing.JFrame;
 
-public class Window extends javax.swing.JFrame{
+public class Window extends javax.swing.JFrame{ 
     public Window(){
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(true);
-        window.setTitle("Tic-tac-toe");
-
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-        window.setSize(100, 100);;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setTitle("Tic-tac-toe");
+        setLocationRelativeTo(null);
     }
 
     public void CreateWindow (){
-        
+        setExtendedState(MAXIMIZED_BOTH);
+        setVisible(true);
     }
 }
