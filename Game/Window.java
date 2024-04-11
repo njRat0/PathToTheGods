b6de831a791e855d33f87e98d3667c8399e0c0cc
njRat0 <oleksandr10001@gmail.com>
2024-04-11 11:40:59 -0400

    menu1

--- Game/Window.java
+++ Game/Window.java
@@ -1,14 +1,17 @@
 package PathToTheGods.Game;
 
 import javax.swing.*;
+
+import org.w3c.dom.css.RGBColor;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window extends javax.swing.JFrame{ 
     public Image gameIcon = Toolkit.getDefaultToolkit().getImage("PathToTheGods\\Images\\Icons\\GameLogo.png");
       
     public Window(){
-        //gameIcon = new Image("/Image/Icons/GameLogo.png");
-        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
         setTitle("PathToTheGods");
@@ -16,6 +19,29 @@ public class Window extends javax.swing.JFrame{
         setExtendedState(MAXIMIZED_BOTH);
         setIconImage(gameIcon);
         setVisible(true);
+    }
+
+    public class Menu extends JPanel implements ActionListener{
+
+        public Menu(){
+            getContentPane().setBackground(Color.DARK_GRAY);
+            JButton test = new JButton();
+            test.setBounds(100, 100, 250, 100);
+            test.addActionListener(this);
+        }
+
+        public void Open(){
+            setVisible(true);
+        }
+
+        public void Close(){
+            setVisible(false);
+        }
+        
+        @Override
+        public void actionPerfomed(ActionEvent e){
+
+        }
     }
 
     
